0e8a9c37271a44cc8b5bf694724f377a993ad27a
Luke Parker [SSW] <10430890+Hona@users.noreply.github.com>
2023-01-28 10:11:13 +1000

    ✨ Report endpoints

---
+++ Jotform/Endpoints/Report/DeleteReport.cs
@@ -0,0 +1,13 @@
+﻿namespace Jotform;
+
+public partial class JotformClient
+{
+    public async Task<JotformResult<string>?> DeleteReportAsync(string reportId, CancellationToken cancellationToken = default)
+    {
+        var response = await _httpClient.DeleteAsync($"report/{reportId}", cancellationToken);
+
+        response.EnsureSuccessStatusCode();
+
+        return await response.Content.ReadFromJsonAsync<JotformResult<string>>(_jsonSerializerOptions, cancellationToken);
+    }
+}
---
+++ Jotform/Endpoints/Report/GetReport.cs
@@ -0,0 +1,8 @@
+﻿namespace Jotform;
+
+public partial class JotformClient 
+{
+    public async Task<JotformResult<FormReport>?> GetReportAsync(string reportId, CancellationToken cancellationToken = default) 
+        => await _httpClient.GetFromJsonAsync<JotformResult<FormReport>>($"report/{reportId}", 
+            _jsonSerializerOptions, cancellationToken);
+}
