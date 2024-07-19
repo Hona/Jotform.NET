9a15fa5d288ddd6324e8428428f85c33ee462b60
Bilal - Mohummed Ibrahim <mohummed.ibrahim@gmail.com>
2024-07-19 15:21:01 +0500

    Updated the TFM to Net 8 + NetStandard 2.0 (for extending support to Net Framework as well).

---
+++ Jotform/Extensions.cs
@@ -0,0 +1,13 @@
+﻿#if NETSTANDARD2_0
+namespace Jotform;
+
+public static class Extensions
+{
+    public static void Deconstruct<T1, T2>(
+        this KeyValuePair<T1, T2> tuple, out T1 key, out T2 value)
+    {
+        key = tuple.Key;
+        value = tuple.Value;
+    }
+}
+#endif
